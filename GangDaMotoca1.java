60568014e9be66953df523ac83ee68e446302dfb
xandelon <161539019+xandelon@users.noreply.github.com>
2024-04-02 14:47:04 -0300

    Update GangDaMotoca1.java

--- GangDaMotoca1.java
+++ GangDaMotoca1.java
@@ -2,6 +2,7 @@ package GDM;
 import robocode.*;
 import robocode.util.Utils;
 import java.awt.Color;
+import java.awt.geom.Point2D;
 import java.util.Random;
 
 public class GangueDaMotoca extends AdvancedRobot {
@@ -12,9 +13,9 @@ public class GangueDaMotoca extends AdvancedRobot {
   public void run() {
     // MudanÃ§a de cor
     setBodyColor(Color.black);
-    setRadarColor(Color.cyan);
-    setGunColor(Color.black);
-    setBulletColor(Color.cyan);
+    setRadarColor(Color.black);
+    setGunColor(Color.red);
+    setBulletColor(Color.red);
 
     // Gira a arma e o radar infinitamente ate achar um inimigo
     setAdjustGunForRobotTurn(true);
@@ -32,12 +33,38 @@ public class GangueDaMotoca extends AdvancedRobot {
     double absBearing = e.getBearingRadians() + getHeadingRadians();
     setTurnRightRadians(Utils.normalRelativeAngle(absBearing - getHeadingRadians() + Math.PI / 2 - moveDirection * Math.PI / 4));
 
-    // Vira a arma para o inimigo
-    setTurnGunRightRadians(Utils.normalRelativeAngle(absBearing - getGunHeadingRadians()));
+    // PrevisÃ£o de movimento linear
+    double bulletPower = Math.min(500 / e.getDistance(), 3.0);
+    double myX = getX();
+    double myY = getY();
+    double enemyX = getX() + e.getDistance() * Math.sin(absBearing);
+    double enemyY = getY() + e.getDistance() * Math.cos(absBearing);
+    double enemyHeading = e.getHeadingRadians();
+    double enemyVelocity = e.getVelocity();
 
-    // Atira baseado na energia do inimigo e na distÃ¢ncia
-    double firePower = Math.min(500 / e.getDistance(), e.getEnergy() / 4);
-    setFire(firePower);
+    double deltaTime = 0;
+    double battleFieldHeight = getBattleFieldHeight(), 
+         battleFieldWidth = getBattleFieldWidth();
+    double predictedX = enemyX, predictedY = enemyY;
+    while((++deltaTime) * (20.0 - 3.0 * bulletPower) < 
+        Point2D.Double.distance(myX, myY, predictedX, predictedY)){		
+      predictedX += Math.sin(enemyHeading) * enemyVelocity;	
+      predictedY += Math.cos(enemyHeading) * enemyVelocity;
+      if(	predictedX < 18.0 
+          || predictedY < 18.0
+          || predictedX > battleFieldWidth - 18.0
+          || predictedY > battleFieldHeight - 18.0){
+        predictedX = Math.min(Math.max(18.0, predictedX), 
+              battleFieldWidth - 18.0);	
+        predictedY = Math.min(Math.max(18.0, predictedY), 
+              battleFieldHeight - 18.0);
+        break;
+      }
+    }
+    double theta = Utils.normalAbsoluteAngle(Math.atan2(predictedX - getX(), predictedY - getY()));
+    setTurnRadarRightRadians(Utils.normalRelativeAngle(absBearing - getRadarHeadingRadians()));
+    setTurnGunRightRadians(Utils.normalRelativeAngle(theta - getGunHeadingRadians()));
+    fire(bulletPower);
 
     // Detecta a perda de energia do inimigo
     double energyDrop = enemyEnergy - e.getEnergy();
@@ -80,5 +107,5 @@ public class GangueDaMotoca extends AdvancedRobot {
 
   // Executa todas as aÃ§Ãµes pendentes
   execute();
-	}
+  }
 }

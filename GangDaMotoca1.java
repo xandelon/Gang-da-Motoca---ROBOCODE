1e3d94455a8dbfaa0d124eb29baa9a83af5db561
xandelon <161539019+xandelon@users.noreply.github.com>
2024-04-29 14:39:40 -0300

    Update GangDaMotoca1.java

--- GangDaMotoca1.java
+++ GangDaMotoca1.java
@@ -3,42 +3,65 @@ import robocode.*;
 import robocode.util.Utils;
 import java.awt.Color;
 import java.awt.geom.Point2D;
+import java.util.HashMap;
 import java.util.Random;
 
-public class GangueDaMotoca extends AdvancedRobot {
-   int moveDirection = 1;
+public class GangueDaMotocax1 extends AdvancedRobot {
+  int moveDirection = 1;
   double enemyEnergy = 100;
   Random random = new Random();
+  HashMap<String, EnemyBot> enemies = new HashMap<String, EnemyBot>();
 
   public void run() {
-    // MudanÃ§a de cor
-    setBodyColor(Color.black);
-    setRadarColor(Color.black);
-    setGunColor(Color.red);
-    setBulletColor(Color.red);
+    
+    setBodyColor(new Color(255, 105, 180));
+    setRadarColor(new Color(255, 105, 180));
+    setGunColor(new Color(255, 105, 180));
+    setBulletColor(new Color(255, 105, 180));
 
-    // Gira a arma e o radar infinitamente ate achar um inimigo
+    
     setAdjustGunForRobotTurn(true);
     setAdjustRadarForGunTurn(true);
 
-    // Movimento padrÃ£o
+    
     while (true) {
-      turnRadarRightRadians(Double.MAX_VALUE); // Movimento de radar padrÃ£o
+      turnRadarRightRadians(Double.MAX_VALUE); 
       execute();
     }
   }
 
   public void onScannedRobot(ScannedRobotEvent e) {
-    // Vira o robÃ´ em direÃ§Ã£o ao inimigo
+    
+    EnemyBot enemy = enemies.get(e.getName());
+    if (enemy == null) {
+      enemy = new EnemyBot();
+      enemies.put(e.getName(), enemy);
+    }
+    enemy.update(e);
+
+    
+    EnemyBot target = chooseTarget();
+
+    
     double absBearing = e.getBearingRadians() + getHeadingRadians();
     setTurnRightRadians(Utils.normalRelativeAngle(absBearing - getHeadingRadians() + Math.PI / 2 - moveDirection * Math.PI / 4));
 
-    // PrevisÃ£o de movimento linear
-    double bulletPower = Math.min(500 / e.getDistance(), 3.0);
+    
+    double distance = e.getDistance();  
+    double bulletPower;
+	
+    if (distance > 500) {	
+        bulletPower = 0.5;
+    } else if (distance > 250) {
+        bulletPower = 1.5;
+    } else {
+        bulletPower = 3.0;
+    }
+
     double myX = getX();
     double myY = getY();
-    double enemyX = getX() + e.getDistance() * Math.sin(absBearing);
-    double enemyY = getY() + e.getDistance() * Math.cos(absBearing);
+    double enemyX = getX() + distance * Math.sin(absBearing);
+    double enemyY = getY() + distance * Math.cos(absBearing);
     double enemyHeading = e.getHeadingRadians();
     double enemyVelocity = e.getVelocity();
 
@@ -66,46 +89,75 @@ public class GangueDaMotoca extends AdvancedRobot {
     setTurnGunRightRadians(Utils.normalRelativeAngle(theta - getGunHeadingRadians()));
     fire(bulletPower);
 
-    // Detecta a perda de energia do inimigo
     double energyDrop = enemyEnergy - e.getEnergy();
     if (energyDrop > 0 && energyDrop <= 3) {
-      // Se o inimigo disparou uma bala, muda de direÃ§Ã£o
+       
       moveDirection = -moveDirection;
       setAhead(50 * moveDirection);
     }
     enemyEnergy = e.getEnergy();
 
-    // Rastreia o inimigo com o radar
     double radarTurn = Utils.normalRelativeAngle(absBearing - getRadarHeadingRadians());
-    double extraTurn = Math.min(Math.atan(36.0 / e.getDistance()), Rules.RADAR_TURN_RATE_RADIANS);
+    double extraTurn = Math.min(Math.atan(36.0 / distance), Rules.RADAR_TURN_RATE_RADIANS);
     if (radarTurn < 0)
       radarTurn -= extraTurn;
     else
       radarTurn += extraTurn;
     setTurnRadarRightRadians(radarTurn);
 
-    // Movimento oscilatÃ³rio
-    if (e.getDistance() > 150) {
-        setAhead((e.getDistance() / 4 + 25) * moveDirection);
+    if (distance > 150) {
+        setAhead((distance / 4 + 25) * moveDirection);
     } else {
-        setBack((e.getDistance() / 4 + 25) * moveDirection);
+        setBack((distance / 4 + 25) * moveDirection);
     }
 
-    // Executa todas as aÃ§Ãµes pendentes
     execute();
   }
 
+  private EnemyBot chooseTarget() {
+    EnemyBot target = null;
+    for (EnemyBot enemy : enemies.values()) {
+      if (target == null || enemy.getEnergy() < target.getEnergy()) {
+        target = enemy;
+      }
+    }
+    return target;
+  }
+
   public void onHitWall(HitWallEvent e) {
-  // Inverte a direÃ§Ã£o do movimento
-  moveDirection = -moveDirection;
 
-  // Vira o robÃ´ para longe da parede
-  setTurnRight(180 + random.nextInt(180) - 90); // Adiciona um Ã¢ngulo aleatÃ³rio entre -90 e 90 graus
+   moveDirection = -moveDirection;
 
-  // Move o robÃ´ para longe da parede
-  setAhead((100 + random.nextInt(200)) * moveDirection); // Adiciona uma distÃ¢ncia aleatÃ³ria entre 100 e 300
+  setTurnRight(90 - e.getBearing()); 
 
-  // Executa todas as aÃ§Ãµes pendentes
+  setAhead(100 * moveDirection); 
+
   execute();
+  }
+
+  public void onHitRobot(HitRobotEvent e) {
+    moveDirection = -moveDirection;
+
+     setTurnRight(90 - e.getBearing());
+
+    execute();
+  }
+}
+
+class EnemyBot {
+  private double distance;
+  private double energy;
+
+  public void update(ScannedRobotEvent e) {
+    distance = e.getDistance();
+    energy = e.getEnergy();
+  }
+
+  public double getDistance() {
+    return distance;
+  }
+
+  public double getEnergy() {
+    return energy;
   }
 }
